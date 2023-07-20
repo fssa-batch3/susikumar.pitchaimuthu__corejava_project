c15ebc850753094bf19d9a57e4155c3bf28c9a46
Susikumar-Pitchaimuthu <susikumar.pitchaimuthu@fssa.freshworks.com>
2023-07-20 12:01:48 +0530

    crpr commit

---
+++ src/main/java/day05/practice/packageOne/AccountTwo.java
@@ -0,0 +1,101 @@
+package day05.practice.packageOne;
+
+public class AccountTwo {
+
+	 private String accNo;
+	    private double balance;
+
+	    // Constructor
+	    public AccountTwo(String accNo, double balance) {
+	        this.accNo = accNo;
+	        this.balance = balance;
+	    }
+
+	    // Getters and setters
+	    public String getAccNo() {
+	        return accNo;
+	    }
+
+	    public void setAccNo(String accNo) {
+	        this.accNo = accNo;
+	    }
+
+	    public double getBalance() {
+	        return balance;
+	    }
+
+	    public void setBalance(double balance) {
+	        this.balance = balance;
+	    }
+	}
+
+	// Interface
+	 abstract class ATM {
+		 
+		 abstract void deposite();
+	    abstract boolean deposit(Account account, double amount);
+	  abstract  boolean withdraw(Account account, double amount) throws Exception;
+	 abstract   double getBalance(Account account); // Pass the Account object as a parameter
+	}
+	 
+	// Implementation 
+	class AxisATM extends ATM {
+	    private static final double WITHDRAWAL_CHARGE = 5;
+
+	    @Override
+	    public boolean deposit(Account account, double amount) {
+	        account.setBalance(account.getBalance() + amount);
+	        return true;
+	    }
+
+	    @Override
+	    public boolean withdraw(Account account, double amount) throws Exception {
+	        if (account.getBalance() < 5000) {
+	            throw new Exception("Insufficient balance. Minimum balance of 5000 required.");
+	        }
+
+	        double totalAmount = amount + WITHDRAWAL_CHARGE;
+	        if (account.getBalance() >= totalAmount) {
+	            account.setBalance(account.getBalance() - totalAmount);
+	            return true;
+	        } else {
+	            throw new Exception("Insufficient balance.");
+	        }
+	    }
+
+	    @Override
+	    public double getBalance(Account account) { // Add the Account object as a parameter
+	        return account.getBalance();
+	    }
+	}
+
+	class IciciATM extends ATM {
+	    private static final double WITHDRAWAL_CHARGE = 10;
+
+	    @Override
+	    public boolean deposit(Account account, double amount) {
+	        account.setBalance(account.getBalance() + amount);
+	        return true;
+	    } 
+
+	    @Override
+	    public boolean withdraw(Account account, double amount) throws Exception {
+	        if (account.getBalance() < 10000) {
+	            throw new Exception("Insufficient balance. Minimum balance of 10000 required.");
+	        }
+
+	        double totalAmount = amount + WITHDRAWAL_CHARGE;
+	        if (account.getBalance() >= totalAmount) {
+	            account.setBalance(account.getBalance() - totalAmount);
+	            return true;
+	        } else {
+	            throw new Exception("Insufficient balance.");
+	        }
+	    }
+
+	    @Override
+	    public double getBalance(Account account) { // Add the Account object as a parameter
+	        return account.getBalance();
+	    }
+
+}
